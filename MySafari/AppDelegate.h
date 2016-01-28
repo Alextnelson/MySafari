1ac2aaf3db101dbfa548d1702a0fa77d4ab31d68
Alextnelson <alexander.thomas.nelson@gmail.com>
2016-01-28 16:21:30 -0600

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Alexander Nelson on 1/28/16.
+//  Copyright © 2016 218 Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Alexander Nelson on 1/28/16.
+//  Copyright © 2016 218 Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
